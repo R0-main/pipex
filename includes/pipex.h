b35d9c1f6d2c6b4c557c26f2dec83e9d12184907
R0-main <justromixx@gmail.com>
2025-01-21 15:59:17 +0100

    working

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,14 +6,21 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 09:52:26 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/01/20 09:53:16 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/01/21 15:58:35 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEXX_H
+# define PIPEXX_H
 
 /******************************************************************************/
-
+typedef struct pipex_data_s
+{
+	int			argc;
+	char const	**argv;
+	char const	**envp;
+	const char	*in_file;
+	const char	*out_file;
+}				pipex_data_t;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:05:25 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/01/21 15:51:43 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/01/21 15:59:05 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 #include "env.h"
+#include "ft_printf.h"
 #include "get_next_line.h"
 #include "libft.h"
 #include <fcntl.h>
@@ -19,43 +20,39 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "pipex.h"
+
+static void	handle_commands(void)
+{
+}
 
 int	main(int argc, char const **argv, char const **envp)
 {
-	int			fd[2];
-	const char	**argt = {&argv[1], &argv[2], NULL};
-	int			exec;
-	int			fd2;
-	char		*in_file;
-	char		*out_file;
-	char		*line;
+	int				fd[2];
+	int				fd2;
+	pipex_data_t	data;
 
-	// char	*args[4] = {"ls", "-R", "", NULL};
-	// if (argc < 4)
-	// {
-	// 	printf("argc error");
-	// 	return (EXIT_FAILURE);
-	// }
+	if (argc < 4)
+	{
+		printf("argc error");
+		return (EXIT_FAILURE);
+	}
 	if (!envp)
 	{
 		printf("envp_error");
 		return (EXIT_FAILURE);
 	}
-	in_file = argv[1];
-	out_file = argv[argc - 1];
+	data.in_file = argv[1];
+	data.out_file = argv[argc - 1];
+	data.argc = argc;
+	data.argv = argv;
+	data.envp = envp;
 	// fd2 = open("test.txt", O_WRONLY | O_APPEND);
 	// close(fd2);
 	// dup2(fd2, 1);
 	// write(1, "fwfq\n\n", 6);
 	// exec = execve("/usr/bin/ls", argt, envp);
 	// printf("===== fwqfqfqfq %d\n", exec);
-	line = malloc(1);
-	while (line)
-	{
-		ft_printf("minishell : ");
-		line = get_next_line(1);
-		// exec_command(ft_split(ft_strtrim(line, "\n"), ' '), envp);
-	}
 	return (0);
 }
 
